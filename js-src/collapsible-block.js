44fb6087dfb0d30eb8c151b63c1aca838a546ce3
Diego Versiani <diegoversiani@gmail.com>
2021-04-29 08:43:10 +0200

    Fix collapse and expand functions to use current height and scroll height to calculate height limits

--- js-src/collapsible-block.js
+++ js-src/collapsible-block.js
@@ -110,7 +110,7 @@
 	 *
 	 * @return  {String}  The transitionend event name
 	 */
-	 var getTransitionEvent = function() {
+	var getTransitionEndEvent = function() {
 		var t;
 		var el = document.createElement('fakeelement');
 		var transitions = {
@@ -126,7 +126,7 @@
 			}
 		}
 
-		return 'animationend';
+		return 'transitionend';
 	};
 
 
@@ -138,13 +138,18 @@
 		if ( e.target.closest( _settings.handlerSelector ) ) {
 			e.preventDefault();
 			
+			// Get target and handler elements
 			var handlerElement = e.target.closest( _settings.handlerSelector );
 			var targetElement = document.querySelector( '#' + handlerElement.getAttribute( _settings.targetAttribute ) );
+			
 			if ( ! targetElement ) {
 				targetElement = handlerElement;
 			}
+
+			// Get the collapsbile element
 			var element = targetElement.closest( _settings.elementSelector );
 
+			// Maybe toggle collapsbile element state
 			if ( element ) {
 				_publicMethods.toggleState( element );
 			}
@@ -163,6 +168,8 @@
 		handler.innerHTML = manager.settings.handlerTemplate.trim();
 		manager.handlerElement = handler.childNodes[0];
 		manager.handlerElement.setAttribute( manager.settings.targetAttribute, contentElement.id );
+		
+		// TODO: Fix button role and behavior should be set with JS instead of using the `href` attribute
 		manager.handlerElement.setAttribute( 'href', '#' );
 		manager.handlerElement.setAttribute( 'role', 'button' );
 
@@ -178,7 +185,6 @@
 		// Bail if content inner element already exists
 		if ( manager.contentElement.querySelector( manager.settings.contentInnerSelector ) ) { return; }
 		
-		var element = manager.element;
 		var contentElement = manager.contentElement;
 		var newContentPlaceholder = document.createElement('div');
 		newContentPlaceholder.innerHTML = manager.settings.contentInnerTemplate.trim();
@@ -187,6 +193,56 @@
 		// Move content to new content inner element
 		contentInner.innerHTML = contentElement.innerHTML;
 		contentElement.innerHTML = newContentPlaceholder.innerHTML;
+	}
+
+
+
+	/**
+	 * Get the element's computed `height` even when hidden or collapsed.
+	 *
+	 * @param   HTMLElement  element  Element to get the computed height value.
+	 *
+	 * @return  Number                The computed height value of the element.
+	 */
+	var getComputedHeight = function( element ) {
+		// Get original element style values
+		var originalPosition = element.style.position;
+		var originalDisplay = element.style.display;
+		var originalVisibility = element.style.visibility;
+		var originalTransition = element.style.transition;
+		var originalHeight = element.style.height;
+
+		// Set element styles prior to getting its height
+		element.style.position = 'absolute';
+		element.style.display = 'block';
+		element.style.visibility = 'hidden';
+		element.style.transition = 'none';
+		element.style.height = '';
+
+		// Get the element's natural height
+		var computedHeight = element.scrollHeight;
+
+		// Set element styles back to original values
+		element.style.position = originalPosition;
+		element.style.display = originalDisplay;
+		element.style.visibility = originalVisibility;
+		element.style.transition = originalTransition;
+		element.style.height = originalHeight;
+
+		return computedHeight;
+	}
+
+
+
+	/**
+	 * Get the element's current used `height` space, even in the middle of a transition.
+	 *
+	 * @param   HTMLElement  element  Element to get the current height value.
+	 *
+	 * @return  Number                The current height value of the element.
+	 */
+	var getCurrentHeight = function( element ) {
+		return element.getBoundingClientRect().height;
 	}
 	
 
@@ -211,6 +267,23 @@
 
 
 	/**
+	 * Remove changed property values from the target element when the `height` transition ends.
+	 */
+	var removeProperties = function ( e ) {
+		// Maybe bail when handling a transition event but not for the right property
+		if ( e && e.propertyName !== 'height' ) return;
+
+		// Remove content element properties when transition is complete
+		e.target.style.height = '';
+		e.target.style.overflow = '';
+
+		// Remove the event handler so it runs only once
+		e.target.removeEventListener( getTransitionEndEvent(), removeProperties );	
+	}
+
+
+
+	/**
 	 * Get slider manager instance from slider element
 	 */
 	_publicMethods.getInstance = function ( element ) {
@@ -220,23 +293,6 @@
 			if ( manager.element == element ) { instance = manager; break; }
 		}
 		return instance;
-	}
-
-
-
-	/**
-	 * Remove property values from the `contentElement` when height transition ends.
-	 */
-	var removeProperties = function ( e ) {
-		if ( e.propertyName !== 'height' ) return;
-
-		// Remove content element properties when transition is complete
-		e.target.style.height = '';
-		e.target.style.overflow = '';
-
-		// Remove the event handler so it runs only once
-		e.target.removeEventListener( getTransitionEvent(), removeProperties );
-		
 	}
 
 
@@ -254,18 +310,20 @@
 		manager.element.classList.add( manager.settings.isCollapsedClass );
 		
 		// Remove `removeProperties` event listener to prevent block from expanding at the end of the transition
-		manager.contentElement.removeEventListener( getTransitionEvent(), removeProperties );
+		manager.contentElement.removeEventListener( getTransitionEndEvent(), removeProperties );
 		
 		// Set content element to hide overflowing content
 		manager.contentElement.style.overflow = 'hidden';
 		
 		requestAnimationFrame( function() {
-			// Set height of the content element to it's current expanded height
-			manager.contentElement.style.height = manager.contentElement.scrollHeight + 'px';
+			// Set height of the element to the current height value
+			// Without knowing the value of `height` property the browser can't calculate the steps of the `height` values
+			// related to the transition time and therefore won't be able to display the transition.
+			manager.contentElement.style.height = getCurrentHeight( manager.contentElement ) + 'px';
 			
 			// Wait for the new height to apply
 			requestAnimationFrame( function() {
-				// Set height of the element to the collapsed state
+				// Set height of the element to the `collapsed` state
 				manager.contentElement.style.height = manager.settings.maxHeight + 'px';
 			});
 		} )
@@ -277,18 +335,31 @@
 	 * Expand element
 	 */
 	_publicMethods.expand = function( element ) {
+		// Get element manager
 		var manager = _publicMethods.getInstance( element );
 
 		// Bail if manager not found
+		// TODO: Maybe try to initialize collapsible and manager on the fly
 		if ( ! manager ) { return; }
 
-		// Set event handler to remove height value when transition ends
-		manager.contentElement.addEventListener( getTransitionEvent(), removeProperties );
+		// Set height of the element to the current height value
+		manager.contentElement.style.height = getCurrentHeight( manager.contentElement ) + 'px';
 
+		// Set event handler to remove height value when transition ends
+		manager.contentElement.addEventListener( getTransitionEndEvent(), removeProperties );
+		
+		// Expand element to its content height
 		requestAnimationFrame( function() {
-			// Expand element to its content height
-			manager.element.classList.remove( manager.settings.isCollapsedClass );
-			manager.contentElement.style.height = manager.contentElement.scrollHeight + 'px';
+			var computedHeight = getComputedHeight( manager.contentElement );
+			
+			// Wait for the new styles to apply
+			requestAnimationFrame( function() {
+				// Set height of the element to the `expanded` state
+				manager.contentElement.style.height = computedHeight + 'px';
+				
+				// Update element's state to `expanded`
+				manager.element.classList.remove( manager.settings.isCollapsedClass );
+			});
 		} );
 	}
 
