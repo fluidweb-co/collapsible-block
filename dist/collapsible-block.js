f9af259bbf344357392ccbf1588016fdf7daff74
Diego Versiani <diego@fluidcheckout.com>
2024-12-17 20:38:13 +0100

    - Fixed: Do not set focus on element initialization.

--- dist/collapsible-block.js
+++ dist/collapsible-block.js
@@ -865,10 +865,10 @@
 		var initialState = initialStateAttribute ? initialStateAttribute : manager.settings.initialState;
 		var index = Array.prototype.indexOf.call( manager.element.parentNode.children, manager.element );
 		if ( initialState == _publicMethods.states.EXPANDED || ( initialState == _publicMethods.states.FIRST_EXPANDED && index == 0 ) ) {
-			_publicMethods.expand( manager.element, false );
+			_publicMethods.expand( manager.element, false, false );
 		}
 		else {
-			_publicMethods.collapse( manager.element, false );
+			_publicMethods.collapse( manager.element, false, false );
 		}
 
 		// Maybe change state on resize
